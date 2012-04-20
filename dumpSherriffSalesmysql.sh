8cadea907b16c0f672942b33dc846ffe46e318d0
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2012-04-19 22:10:39 -0400

    updated database, cleaned things up some

--- dumpSherriffSalesmysql.sh
+++ dumpSherriffSalesmysql.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-outputfilename="SheriffSalesDumpLatest.out"
+dbuser="nicolae"
+
+if [ $# -ne 1 ]
+then
+    outputfilename="SheriffSalesDumpLatest.out"
+else 
+    outputfilename="$1"
+fi
 while [ -e $outputfilename ]
 do 
-echo "$outputfilename exists. \nPlease enter an alternate output filename:"
-read outputfilename
+    echo "$outputfilename exists. \nPlease enter an alternate output filename:"
+    read outputfilename
 done
- 
+
 echo "Writing database dump to $outputfilename"
 
-mysqldump -u nicolae -p SheriffSales > "$outputfilename"
+echo "Logging into the local database as $dbuser."
+mysqldump -u "$dbuser" -p SheriffSales > "$outputfilename"
