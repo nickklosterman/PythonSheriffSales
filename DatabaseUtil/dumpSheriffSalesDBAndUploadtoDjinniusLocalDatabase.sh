dedf2963de5cbb93953394f7758b62e9212102c2
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2012-07-09 13:34:21 -0400

    new data and improved DB dumper/uploader to auto update filename

--- DatabaseUtil/dumpSheriffSalesDBAndUploadtoDjinniusLocalDatabase.sh
+++ DatabaseUtil/dumpSheriffSalesDBAndUploadtoDjinniusLocalDatabase.sh
@@ -1,20 +1,68 @@
 #!/bin/bash
+function getFileExtension()
+{
+    extension1=${1##*.}
+    extension=${extension1,,} #convert to all lower case                                                                                                                                                     
+    echo "$extension"
+}
+function getFileName()
+{
+    filename=${1%.*}
+    echo "$filename"
+}
+
+function createUniqueFilename()
+{
+    filename="${1}"
+#    filenamebase=${filename%.*}
+#    filenameextension=${filename#*.}
+    filenamebase=$( getFileName $filename )
+    filenameextension=$( getFileExtension $filename )
+    temp=$filenamebase.$filenameextension
+    number=0
+    
+    while [[ -e "$filename" ]]
+    do
+#	echo $number
+        let 'number+=1'
+        numberformat=$( printf "%02d" $number )
+        filename=${filenamebase}_$numberformat.${filenameextension}
+    done
+    touch "$filename" #needed more for 
+    echo "$filename"
+}
+
 dbuserLocal="nicolae"
 dbuserRemote="djinnius"
+todaysdate=`date +%F`
 if [ $# -ne 1 ]
 then
-    outputfilename="SheriffSalesDumpLatest.out"
+    outputfilename="SheriffSales-$todaysdate.out"
 else 
     outputfilename="$1"
 fi
-while [ -e $outputfilename ]
-do 
-    echo "$outputfilename exists. \nPlease enter an alternate output filename:"
-    read outputfilename
-done
+
+
+#getFileExtension $outputfilename
+#getFileName $outputfilename
+
+
+#createUniqueFilename $outputfilename 
+outputfilename=$( createUniqueFilename $outputfilename )
+
+if [ 0 -eq 1 ]
+then #old method of preventing filename collisions
+    while [ -e $outputfilename ]
+    do 	
+	echo "$outputfilename exists. \nPlease enter an alternate output filename:"
+	read outputfilename
+	
+    done
+fi
 
 echo "Writing database dump to $outputfilename"
-
+if [ 0 -eq 1 ]
+then
 echo "Logging into the local database as $dbuserLocal."
 mysqldump -u "$dbuserLocal" -p SheriffSales > "$outputfilename"
 
@@ -28,3 +76,4 @@ then
     echo "uploading database $outputfilename"
 fi
 #have to do it as djinnius bc otherone doesn't have drop acces...could easily change..
+fi
