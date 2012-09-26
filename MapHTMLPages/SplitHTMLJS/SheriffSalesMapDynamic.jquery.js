2cfb778f48b469c88c156ca3e7f2f62d133869d1
U-INTELLIGRATED\nick.klosterman <nick.klosterman@MAS002326-LAP.intelligrated.com>
2012-09-26 16:29:10 -0400

    corrected the displaying x to y of z records message. seems downloadurl doesnt complete serially but other functions are executing before downloadurl is done. using setTimeout didnt work either

--- MapHTMLPages/SplitHTMLJS/SheriffSalesMapDynamic.jquery.js
+++ MapHTMLPages/SplitHTMLJS/SheriffSalesMapDynamic.jquery.js
@@ -2,21 +2,29 @@ var offset=0, recordstodisplay, curr_record =0;
 
 $(document).ready(function(){
 		      load();
-//		      updateMap();
-		      getRecordsValues();
-		      writeout();
+//                      updateMap();
+//		      writeout();
 
 //		      $("#dec").hide(); //initially we want to not allow the user to
  		      $("#incrementOffset").click(function(){
 						      getRecordsValues();
 						      curr_record +=  recordstodisplay;
 						      console.log("curr_record:",curr_record,"recordCount:",recordCount);
+//have option to turn off this correction such that the 
 						      if (curr_record+recordstodisplay > recordCount)
 						      {
-							  curr_record = recordCount-recordstodisplay;
+                                                          console.log("what");
+                                                          curr_record = recordCount-recordCount%recordstodisplay; //this will cause it such that if the recordCount isn't a multiple of recordstodisplay that only the last fraction of the records is displayed. ie if the query returns 8 records and we are dislay ing 5 at a time the last set will display 3 , records 6,7,8
+    // curr_record = recordCount%recordstodisplay; //this will cause the last records to display recordstodisplay last records. ie we would display records 4,5,6,7,8 for the last records using the example above.
+
+							//  curr_record = recordCount-recordstodisplay;
 							  //$("#inc").hide();
 						      }
 						      
+                                                      if (curr_record < 0 )
+                                                      {
+                                                          curr_record = 0;
+                                                      }
 						      $("#dec").show();
 						      updateMap( );
 						      offset = curr_record ;
@@ -26,7 +34,11 @@ $(document).ready(function(){
 		      $("#decrementOffset").click(function(){
 						      getRecordsValues();
 						      curr_record -= (recordstodisplay) ; 
-						      
+	                                              if (curr_record < 0 )
+					              {
+                                                          curr_record = 0
+                                                      }
+
 						      if ( curr_record-recordstodisplay < 0.1 )
 						      {
 							  curr_record = 0 ;
@@ -52,9 +64,30 @@ $(document).ready(function(){
 
 function writeout(){
     var rangelo = (curr_record+1);
+    recordstodisplay = parseInt(document.getElementById('recordstodisplay').value);
     var rangehi = (curr_record+recordstodisplay);
-//    $("#CurrentRecordsDisplayed").val( rangelo.toString()+"-"+rangehi.toString()); 
-    $("#CurrentRecordsDisplayed").html("Displaying "+ rangelo.toString()+"-"+rangehi.toString()+" of "+recordCount+" matching records."); 
+    if (rangehi > recordCount)
+    {
+        rangehi = recordCount;
+    }
+    //    $("#CurrentRecordsDisplayed").val( rangelo.toString()+"-"+rangehi.toString()); 
+    if ( recordCount > 0 )  
+    {
+        if ( recordstodisplay < recordCount )
+        { 
+            console.log("recordstodisplay:",recordstodisplay," recordCount:",recordCount);
+            $("#CurrentRecordsDisplayed").html("Displaying "+ rangelo.toString()+"-"+rangehi.toString()+" of "+recordCount+" matching records.");     
+        }
+        else {
+            console.log("recordstodisplay:",recordstodisplay," recordCount:",recordCount);
+            $("#CurrentRecordsDisplayed").html("Displaying all matching records.");     
+        }
+    }
+    else
+    {
+        $("#CurrentRecordsDisplayed").html("<h3 style=\"color:red\" >There are no records to display using your criteria.</h3>"); 
+    }
+    
     console.log("Displaying "+ rangelo.toString()+"-"+rangehi.toString()+" of "+recordCount+" matching records."); 
 };
 
--- MapHTMLPages/SplitHTMLJS/SheriffSalesMapDynamic.js
+++ MapHTMLPages/SplitHTMLJS/SheriffSalesMapDynamic.js
@@ -106,10 +106,13 @@ function load() {
     infoWindow = new google.maps.InfoWindow;
 
     //    queryString = "http://djinnius.com/SheriffSales/Sandbox/phpdatabasequery.php?maxbid=2000000&minbid=1000&salestatus=*&saledate=*&pricefiltercategory=Appraisal&table=Property&recordsoffset=0&recordstodisplay=50"; //must have spaces in btw + "" otherwise you break it!"; //must have spaces in btw + "" otherwise you break it!
-    queryString = "http://djinnius.com/SheriffSales/Sandbox/phpdatabasequery.php?maxbid=2000000&minbid=1000&salestatus=*&saledate=*&pricefiltercategory=MinBid&table=Property&recordsoffset=0&recordstodisplay=50"; //must have spaces in btw + "" otherwise you break it!"; //must have spaces in btw + "" otherwise you break it!
+    queryString = "http://djinnius.com/SheriffSales/Sandbox/phpdatabasequery.php?maxbid=2000000&minbid=1000&salestatus=*&saledate=*&pricefiltercategory=MinBid&table=Property&recordsoffset=0&recordstodisplay=205"; //must have spaces in btw + "" otherwise you break it!"; //must have spaces in btw + "" otherwise you break it!
     console.log("this query isn't pulling the variables from the inputs, its just hardcoded. this is one reason I wanna use updateMap() solely");
     console.log("load:",queryString);
+
     getRecordCountOfQuery(queryString);
+//    getRecordCountOfQuery(queryString);
+
     downloadUrl(queryString, function(data) {
 		    xml = data.responseXML;
 		    markers = xml.documentElement.getElementsByTagName("marker");
@@ -162,6 +165,7 @@ function load() {
 		    }
 		});
     console.log("I would really like to figure out how to use clearLocations() without it barfing when markersArr isn't populated. That way I can get rid of load() and just use updateMap().");
+
 }
 
 
@@ -195,8 +199,19 @@ function updateMap() {
     downloadUrl( queryString, function(data) {
 		     xml = data.responseXML;
 		     markers = xml.documentElement.getElementsByTagName("marker");
-		     clearLocations(); // if I could figure out how to 
-		     //		     markersArr= new Array();
+                     if (firstRun !== true )
+                     {
+                         
+                         
+		         clearLocations(); // if I could figure out how to 
+                         
+		         //		     markersArr= new Array();
+                     }
+                     else
+                     {
+                         
+                         console.log("firstRun :",firstRun);
+                     }
 		     for ( i = 0; i < markers.length; i++) {
 			 SaleDate = markers[i].getAttribute("SaleDate");
 			 CaseNumber = markers[i].getAttribute("CaseNumber");
@@ -242,6 +257,7 @@ function updateMap() {
 			 markersArr.push(marker);
 		     }
 		 });
+    firstRun = false;
 }
 
 
@@ -249,13 +265,17 @@ function getRecordCountOfQuery(oldQueryString) {
     var xml, record ;
     var queryString = oldQueryString.replace(/phpdatabasequery/g, "phpgetrecordcount")
     console.log("getRecordCountOfQuery:",queryString);
+    console.log("why doesn't it enter downloadUrl now?? I'm guessing that there is some sort of latency for downloadUrl to complete such that other functions are completed as we wait for downloadUrl to complete.");
     downloadUrl( queryString,function(data)
 		 {
+                     console.log("inside downloadUrl, is it just a delayed completion??");
 		     xml = data.responseXML; 
 		     record = xml.documentElement.getElementsByTagName("data");
 		     console.log("record",record);
 		     recordCount = record[0].getAttribute("recordCount");
+                     console.log("recordCount in downloadUrl:",recordCount);
+                     writeout();
 		 });
-    console.log("from what I can debug, it's a problem with the JS and not php as the php returns a valid xml item with a value. I do need to look into the diff btw what the mydql workbench is returning and the +1 value php is returning. I think it is because I'm using <= and >=. Yeah that appears to be it.")
-    console.log("recordCount:",recordCount);
+//    console.log("from what I can debug, it's a problem with the JS and not php as the php returns a valid xml item with a value. I do need to look into the diff btw what the mydql workbench is returning and the +1 value php is returning. I think it is because I'm using <= and >=. Yeah that appears to be it.")
+    console.log("recordCount at exit:",recordCount);
 }
