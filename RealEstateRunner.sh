d6cf0612e62133c9c7aef8294304b607191daf6a
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2014-03-25 22:28:53 -0400

    update of Hamilton cty for newer data layout

--- RealEstateRunner.sh
+++ RealEstateRunner.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 cd ~/Git/PythonSheriffSales/RealEstateSalesData/MontgomeryCountyOhio
-bash Get2013UnzipResSales.sh
-cd ~/Git/PythonSheriffSales/
-python ProcessRealEstateSalesMontgomeryCountyOhio2013.py
+#bash Get2013UnzipResSales.sh
+year=` date +%Y `
+
+ bash GetXUnzipResSales.sh $year
+
+ cd ~/Git/PythonSheriffSales/
+ #python ProcessRealEstateSalesMontgomeryCountyOhio2013.py
+ python ProcessRealEstateSalesMontgomeryCountyOhio2014.py
+
 cd ~/Git/PythonSheriffSales/Geocoders 
-python GeocodeRealEstateDatabase.py
+python GeocodeRealEstateDatabase2014.py
--- SheriffSaleProcessors/SheriffSalesLinkCreatorHamiltonCountyOhio.py
+++ SheriffSaleProcessors/SheriffSalesLinkCreatorHamiltonCountyOhio.py
@@ -10,13 +10,13 @@ def translatetownship(name):
         "BLUE ASH":"Blue Ash",
         "CHEVIOT":"Cheviot",
         "CINTI":"Cincinnati",
-                   "CINCI":"Cincinnati",
+        "CINCI":"Cincinnati",
         "CINC":"Cincinnati",
         "CLEVES":"Cleve",
         "COLERAIN TWSP":"Colerain Township",
         "COLLEGE HILL":"College Hill",
         "COLUMBIA TWSP":"Columbia Township",
-                   "CROSBY TWSP":"Crosby Township",
+        "CROSBY TWSP":"Crosby Township",
         "DEER PARK":"Deer Park",
         "DELHI TWSP":"Delhi Township",
         "ELMWOOD PL":"Elmwood Place",
@@ -25,6 +25,7 @@ def translatetownship(name):
         "FOREST PARK":"Forest Park",
         "GLENDALE":"Glendale",
         "GOLF MANOR":"Golf Manor",
+        "GREEN":"Green",
         "GREEN TWSP":"Green Township",
         "GREENHILLS":"Greenhills",
         "HARRISON":"Harrison",
@@ -66,6 +67,9 @@ def striphtml(data):
 def striptdfontend(data):
     p = re.compile(r'</font></td>')
     return p.sub('', data)
+def striptdend(data):
+    p = re.compile(r'</td>')
+    return p.sub('', data)
 def stripnowrap(data):
     p = re.compile(r' nowrap="nowrap"')
     return p.sub('',data)
@@ -73,7 +77,7 @@ def stripspaces(data):
     p = re.compile(r' ')
     return p.sub('+',data)
 def stripampersand(data):
-    p = re.compile(r'&')
+    p = re.compile(r'&amp;')
     return p.sub('and',data) # &amp 
 def stripcomma(data):
     p = re.compile(r',')
@@ -159,38 +163,30 @@ def ProcessFile(inputfilename,outputfilename):
 #although keying off line # works, it isn't foolproof. yet there aren't tags to truly key off of either. 
 
 #Hamilton Cty Data:
-#headers on line 297
-#records start on line 299
+#headers on line 210
+#records start on line 211
     for line in AuctionListFile:
-        if (linecounter > 298) and (enddataflag==0): 
+
+        if (linecounter > 210) and (enddataflag==0): 
+#            print(line) 
             line1=line.strip()
             linelower=line.lower()
-#could simply go off of line that we are on as 1st line holds most property data, 2nd line states whether property still for sale, 3rd,4th are formatting
-            if line.find("<td><font face=\"Trebuchet MS\" size=\"2\">")!=-1:
+            #data rows have td in them.
+            if line.find("<td>")!=-1:
                 parseddata=ParseTableData1(line.strip())
-                propertyRecordcounter=0
-            if propertyRecordcounter==1:
-                if linelower.find("checked=\"checked\"")!=-1: 
-                    SaleStatus="WITHDRAWN"
-                else:
-                    SaleStatus="ACTIVE"
-
-            propertyRecordcounter+=1
-
-            if line.find("</tr><tr>")!=-1: #we've reached the end of a record, format output and reset counter
-                propertyRecordcounter=0 #reset counter when outside a record                                                  
-                CaseNumber=parseddata[1]
-                Plaintiff=parseddata[2]
-                Defendant=parseddata[3]
-                Address=parseddata[4]+" "+translatetownship(parseddata[5])+" Ohio"
-                Attorney=parseddata[6]
-                Appraisal=prepprice(parseddata[8])
-                MinBidAmt=prepprice(parseddata[9])
-                SaleDate=convertDateFormatDMYSlashdelimited(parseddata[10])
-                #print(CaseNumber,Plaintiff,Defendant,Address,Attorney,appraisal,minbidamt,SaleDate)
-#                print(SaleStatus)
+                #print(parseddata)
+#2H['<td>A1307095', 'J P MORGAN CHASE BANK', 'AHERN, STEPHANIE', '6740 PARKLAND AVE', 'DENNIS REIMER', '440-600-5500', '$60,000', '$40,000', 'CINTI', 0, '3/27/2014</td>'
+                CaseNumber=parseddata[0][4:] #remove <td> at front
+                Plaintiff=parseddata[1]
+                Defendant=parseddata[2]
+                Address=parseddata[3]+" "+translatetownship(parseddata[8])+" Ohio"
+                Attorney=parseddata[4]
+                Appraisal=prepprice(parseddata[6])
+                MinBidAmt=prepprice(parseddata[7])
+                SaleDate=convertDateFormatDMYSlashdelimited(parseddata[10][0:-5]) #remove </td> at end.
+                SaleStatus=parseddata[9]
+                #print(CaseNumber,Plaintiff,Defendant,Address,Attorney,Appraisal,MinBidAmt,SaleDate,SaleStatus)
                 if 1==1:
-#CaseNumber,Plaintiff,Name(defendant),Address,township,Attorney,AttyPhone,Appraisal,MinimumBid,SaleDate,Withdrawn
                     #print(CaseNumber,Plaintiff,Defendant,Address,Attorney,appraisal,minbidamt,SaleDate)
                     key=QueryDatabaseIfRecordExists(SaleDate,CaseNumber,Address, Plaintiff,Defendant,Attorney,SoldTo,PID,Zipcode,Appraisal,MinBidAmt,SaleAmt,SaleStatus) 
                     #key=99
@@ -292,17 +288,17 @@ def ProcessSalesFile(inputfilename,outputfilename):
     output.write(data)
     output.close()
 
+#remove table tags and split fields
 def ParseTableData1(line):
-    stripped=striptdfontend(line)
-    one=line.replace(" align=\"Center\"","")
-    two=one.replace(" align=\"Right\"","")
-    three=two.replace("</font></td>","")
-    four=three.replace(" nowrap=\"nowrap\"","")
+    removedtdend=stripampersand(line) 
+    separated=removedtdend.split("</td><td>")
+    #    print(separated)#[11],separated[10])
+    if (separated[9]).find("checked")!=-1:
+        separated[9]=1
+    else:
+        separated[9]=0
     
-    separated=four.split("<td><font face=\"Trebuchet MS\" size=\"2\">")
-    if separated[8]=='&nbsp;':
-        separated[8]="NONE"
-#    print separated
+    #print separated
     return separated
 
 #attempt to parse html
@@ -335,7 +331,9 @@ def convertDateFormat(date):
     return dt.strftime("%Y-%m-%d")
 
 def convertDateFormatDMYSlashdelimited(date):
+    #print(date)
     datesplit=date.split("/")
+    #print(datesplit)
     day=int(datesplit[1])
     month=int(datesplit[0])
     year=int(datesplit[2])
@@ -343,7 +341,9 @@ def convertDateFormatDMYSlashdelimited(date):
     return dt.strftime("%Y-%m-%d")
 
 def convertDateFormatMDYSlashdelimited(date):
+    #print(date)
     datesplit=date.split("/")
+    #print(datesplit)
     day=int(datesplit[1])
     month=int(datesplit[0])
     year=int(datesplit[2])
