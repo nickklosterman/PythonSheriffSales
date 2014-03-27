c10ea8a5e5b574c4e2fd1afb0abbe02961b253a5
nickklosterman <nick_klosterman@yahoo.com>
2014-03-27 16:39:51 -0400

    added more params to be passed

--- GetHamiltonCountySheriffSalesList.sh
+++ GetHamiltonCountySheriffSalesList.sh
@@ -23,7 +23,8 @@ txtAddress=""
 btnCurrent="Upcoming Foreclosures"
 txtCaseno=""
 txtAddress=""
-curl -d "txtAddress=${txtAddress}&txtCaseno=${txtCaseno}&ddlDate=${ddlDate}&ddlTown=${ddlTown}&__EVENTVALIDATION=${eventValidation}&__VIEWSTATEENCRYPTED=${viewStateEncrypted}&__EVENTTARGET=${eventTarget}&__EVENTARGUMENT=${eventArgument}&_TSM_HiddenField=${tsm_HiddenField}__VIEWSTATE=${viewState}&txtAddress_TextBoxWatermarkExtender_ClientState=${txtAddress}&btnCurrent=${btnCurrent}" http://apps.hcso.org/PropertySale.aspx > "${outputFileName}"
+btnGo="GO"
+curl -d "txtAddress=${txtAddress}&txtCaseno=${txtCaseno}&ddlDate=${ddlDate}&ddlTown=${ddlTown}&__EVENTVALIDATION=${eventValidation}&__VIEWSTATEENCRYPTED=${viewStateEncrypted}&__EVENTTARGET=${eventTarget}&__EVENTARGUMENT=${eventArgument}&_TSM_HiddenField=${tsm_HiddenField}__VIEWSTATE=${viewState}&txtAddress_TextBoxWatermarkExtender_ClientState=${txtAddress}&btnCurrent=${btnCurrent}&btnGo=${btnGo}" http://apps.hcso.org/PropertySale.aspx > "${outputFileName}"
 
 #curl -d "__EVENTVALIDATION=${eventValidation}&__VIEWSTATEENCRYPTED=${viewStateEncrypted}&__EVENTTARGET=${eventTarget}&__EVENTARGUMENT=${eventArgument}&_TSM_HiddenField=${tsm_HiddenField}__VIEWSTATEENCRYPTED=${viewStateEncrypted}&txtAddress_TextBoxWatermarkExtender_ClientState=${txtAddress}&btnCurrent=${btnCurrent}" http://apps.hcso.org/PropertySale.aspx > "${outputFileName}"
 
