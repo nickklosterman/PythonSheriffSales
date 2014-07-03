a03749ed092cf98395d9dcf1fb27462979672ffe
nickklosterman <nick_klosterman@yahoo.com>
2014-07-03 17:01:48 -0400

    updated curl command

--- GetHamiltonCountySheriffSalesList.sh
+++ GetHamiltonCountySheriffSalesList.sh
@@ -24,7 +24,7 @@ btnCurrent="Upcoming Foreclosures"
 txtCaseno=""
 txtAddress=""
 btnGo="GO"
-curl -d "txtAddress=${txtAddress}&txtCaseno=${txtCaseno}&ddlDate=${ddlDate}&ddlTown=${ddlTown}&__EVENTVALIDATION=${eventValidation}&__VIEWSTATEENCRYPTED=${viewStateEncrypted}&__EVENTTARGET=${eventTarget}&__EVENTARGUMENT=${eventArgument}&_TSM_HiddenField=${tsm_HiddenField}__VIEWSTATE=${viewState}&txtAddress_TextBoxWatermarkExtender_ClientState=${txtAddress}&btnCurrent=${btnCurrent}&btnGo=${btnGo}" http://apps.hcso.org/PropertySale.aspx > "${outputFileName}"
+curl -A "Mozilla" -d "txtAddress=${txtAddress}&txtCaseno=${txtCaseno}&ddlDate=${ddlDate}&ddlTown=${ddlTown}&__EVENTVALIDATION=${eventValidation}&__VIEWSTATEENCRYPTED=${viewStateEncrypted}&__EVENTTARGET=${eventTarget}&__EVENTARGUMENT=${eventArgument}&_TSM_HiddenField=${tsm_HiddenField}__VIEWSTATE=${viewState}&txtAddress_TextBoxWatermarkExtender_ClientState=${txtAddress}&btnCurrent=${btnCurrent}&btnGo=${btnGo}" http://apps.hcso.org/PropertySale.aspx > "${outputFileName}"
 
 #curl -d "__EVENTVALIDATION=${eventValidation}&__VIEWSTATEENCRYPTED=${viewStateEncrypted}&__EVENTTARGET=${eventTarget}&__EVENTARGUMENT=${eventArgument}&_TSM_HiddenField=${tsm_HiddenField}__VIEWSTATEENCRYPTED=${viewStateEncrypted}&txtAddress_TextBoxWatermarkExtender_ClientState=${txtAddress}&btnCurrent=${btnCurrent}" http://apps.hcso.org/PropertySale.aspx > "${outputFileName}"
 
